2a7434037ad379df12d755372b607b89b4a103e3
Hindia <victorhindia@gmail.com>
2024-06-29 22:16:47 +0300

    adding permissions and media root

--- main/views.py
+++ main/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin 
 from . models import Books
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
-class BooksView(ListView):
+class BooksView(LoginRequiredMixin,ListView):
     model=Books
     template_name='book.html'
 
-class BookDetail(DetailView):
+class BookDetail(LoginRequiredMixin,DetailView):
     model=Books
     template_name='detail.html'
